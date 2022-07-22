1a552c7567393f67c8c104aff74a59db8a8ea191
Sriganesh2000 <shreevj2910@gmail.com>
2022-07-22 16:03:46 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,31 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'ANGULAR ' + VERSION.major;
+  public headers = ['Name', 'Subject', 'Mark'];
+  public rows = [
+    {
+      Name: 'Ramesh',
+      Subject: 'English',
+      Mark: '71',
+    },
+    {
+      Name: 'Suresh',
+      Subject: 'English',
+      Mark: '80',
+    },
+    {
+      Name: 'Ramesh',
+      Subject: 'Science',
+      Mark: '78',
+    },
+    {
+      Name: 'Suresh',
+      Subject: 'Science',
+      Mark: '88',
+    },
+  ];
 }
